3f11e9b484b8a90250b5b864f822f9f7f179759b
Olivia Weston <olivia@weston.enterprises>
2024-02-08 14:20:44 +0000

    Attempt at making binary operators work, gives warnings

--- lib/stlc.ml
+++ lib/stlc.ml
@@ -2,6 +2,9 @@ type livTyp = Integer
             | Boolean
             | Arrow of livTyp * livTyp 
 
+type livBinOp = Plus | Minus | Mult | Div
+                | Lt | Le | Gt | Ge | Eq | Neq
+
 type livConst = CInteger of int
               | CBoolean of bool
 
@@ -13,3 +16,5 @@ type livTerm = TConstant of livConst
              | TVariable of livVar
              | TAbstract of livBinder * livTyp * livTerm
              | TApplication of livTerm * livTerm
+             | TBinOp of livBinOp * livTerm * livTerm
+             (* Technically binary operators are constants in the language *)
