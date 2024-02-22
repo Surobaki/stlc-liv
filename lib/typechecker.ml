e429712e66d88a2f89e7696b323b0e0fef6f2034
Olivia Weston <olivia@weston.enterprises>
2024-02-22 14:43:23 +0000

    Fixed up typechecker

--- lib/typechecker.ml
+++ lib/typechecker.ml
@@ -1,49 +1,50 @@
+open Stlc
+
 let _APP_ARG_ERR = Errors.Type_error ("Failed to match argument of function with function signature.")
 let _NONFUNC_APP_ERR = Errors.Type_error ("Non-functional application.") 
 let _OP_ARG_ERR = Errors.Type_error ("Failed to match argument of operator with expected type.")
+let _CTXT_VAR_NOT_FOUND_ERR = Errors.Type_error ("Failed to match provided variable with a variable in the context.")
 
 let rec typecheck env tm = 
   match tm with
-  | TConstant con -> match con with
-                     | CInteger _ -> Integer
-                     | CBoolean _ -> Boolean
+  | TConstant con -> (match con with
+                      | CInteger _ -> Integer
+                      | CBoolean _ -> Boolean)
   | TVariable var -> 
-    snd (List.find 
-          (fun elem -> match elem with
-          | (srchVar, _) -> var == srchVar
-          | _ -> false) 
-        env)
+    (match (List.assq_opt var env) with
+     | Some typ -> typ
+     | None -> raise _CTXT_VAR_NOT_FOUND_ERR)
   | TAbstract (bind, typ, tm') -> 
-    let enrichedEnv = (cons (bind, typ) env) in 
+    let enrichedEnv = (List.cons (bind, typ) env) in 
     let tm'Typ = typecheck enrichedEnv tm' in
       Arrow (typ, tm'Typ)
   | TApplication (tm1, tm2) -> 
-      let tm1Typ = typecheck tm1 in
-      let tm2Typ = typecheck tm2 in
-        match tm1Typ with
-        | Arrow (firstArg, _) -> 
-               if firstArg == tm2Typ then
-               Arrow (tm1Typ, tm2Typ)
-               else
-               raise APP_ARG_ERR
-        | _ -> raise NONFUNC_APP_ERR
+      let tm1Typ = typecheck env tm1 in
+      let tm2Typ = typecheck env tm2 in
+        (match tm1Typ with
+         | Arrow (firstArg, _) -> 
+                if firstArg == tm2Typ then
+                Arrow (tm1Typ, tm2Typ)
+                else
+                raise _APP_ARG_ERR
+         | _ -> raise _NONFUNC_APP_ERR)
   | TBinOp (op, tm1, tm2) ->
-    match op with
-    | Plus | Minus | Mult | Div ->
-      let tm1Typ = typecheck tm1 in
-      let tm2Typ = typecheck tm2 in
-        match tm1Typ, tm2Typ with
-        | Integer, Integer -> Integer
-        | _ -> raise _OP_ARG_ERR
-    | Lt | Le | Gt | Ge ->
-      let tm1Typ = typecheck tm1 in
-      let tm2Typ = typecheck tm2 in
-        match tm1Typ, tm2Typ with
-        | Integer, Integer -> Boolean
-        | _ -> raise _OP_ARG_ERR
-    | Eq | Neq ->
-      let tm1Typ = typecheck tm1 in
-      let tm2Typ = typecheck tm2 in
-        if tm1Typ == tm2Typ then Boolean
-                            else raise _OP_ARG_ERR
+    (match op with
+     | Plus | Minus | Mult | Div ->
+       let tm1Typ = typecheck env tm1 in
+       let tm2Typ = typecheck env tm2 in
+         (match tm1Typ, tm2Typ with
+          | Integer, Integer -> Integer
+          | _ -> raise _OP_ARG_ERR)
+     | Lt | Le | Gt | Ge ->
+       let tm1Typ = typecheck env tm1 in
+       let tm2Typ = typecheck env tm2 in
+         (match tm1Typ, tm2Typ with
+          | Integer, Integer -> Boolean
+          | _ -> raise _OP_ARG_ERR)
+     | Eq | Neq ->
+       let tm1Typ = typecheck env tm1 in
+       let tm2Typ = typecheck env tm2 in
+         if tm1Typ == tm2Typ then Boolean
+                             else raise _OP_ARG_ERR)
                    
