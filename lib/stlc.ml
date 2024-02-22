0d84a75d11a9d42e1ddac86b2636120741078304
Olivia Weston <olivia@weston.enterprises>
2024-02-22 12:13:58 +0000

    Added ppx_deriving to STLC ASTs

--- lib/stlc.ml
+++ lib/stlc.ml
@@ -1,16 +1,21 @@
 type livTyp = Integer
             | Boolean
             | Arrow of livTyp * livTyp 
+            [@@deriving show]
 
 type livBinOp = Plus | Minus | Mult | Div
                 | Lt | Le | Gt | Ge | Eq | Neq
+                [@@deriving show]
 
 type livConst = CInteger of int
               | CBoolean of bool
+              [@@deriving show]
 
 type livVar = string
+              [@@deriving show]
 
 type livBinder = string
+                 [@@deriving show]
 
 type livTerm = TConstant of livConst
              | TVariable of livVar
@@ -18,3 +23,7 @@ type livTerm = TConstant of livConst
              | TApplication of livTerm * livTerm
              | TBinOp of livBinOp * livTerm * livTerm
              (* Technically binary operators are constants in the language *)
+             [@@deriving show]
+
+type livEnv = (livVar * livTyp) list 
+              [@@deriving show]
