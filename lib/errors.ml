3c961fbb2447e4648cfb867845867cf7017f29f5
Olivia Weston <olivia@weston.enterprises>
2024-03-05 15:18:04 +0000

    Runtime error in prep for evaluation

--- lib/errors.ml
+++ lib/errors.ml
@@ -1,6 +1,7 @@
 exception Parse_error of string
 exception Syntax_error of string
 exception Type_error of string
+exception Runtime_error of string
     
 let print_raise_exn excep = print_endline (Printexc.to_string excep);
                             raise excep
