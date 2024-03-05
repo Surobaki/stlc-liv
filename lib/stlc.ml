3304a4e4eaaf11cabb0d1d4c4dc955a50aa37d35
Olivia Weston <olivia@weston.enterprises>
2024-03-05 15:17:46 +0000

    Minor comments in STLC for clarity

--- lib/stlc.ml
+++ lib/stlc.ml
@@ -1,22 +1,28 @@
+(* STLC type universe *)
 type livTyp = Integer
             | Boolean
             | Arrow of livTyp * livTyp 
             [@@deriving show]
 
+(* STLC binary operations *)
 type livBinOp = Plus | Minus | Mult | Div
                 | Lt | Le | Gt | Ge | Eq | Neq
                 [@@deriving show]
 
+(* STLC constants *)
 type livConst = CInteger of int
               | CBoolean of bool
               [@@deriving show]
 
+(* STLC variables *)
 type livVar = string
               [@@deriving show]
 
+(* Binders of lambdas *)
 type livBinder = string
                  [@@deriving show]
 
+(* Terms of STLC *)
 type livTerm = TConstant of livConst
              | TVariable of livVar
              | TAbstract of livBinder * livTyp * livTerm
@@ -25,5 +31,6 @@ type livTerm = TConstant of livConst
              (* Technically binary operators are constants in the language *)
              [@@deriving show]
 
+(* Typing environment *)
 type livEnv = (livVar * livTyp) list 
               [@@deriving show]
